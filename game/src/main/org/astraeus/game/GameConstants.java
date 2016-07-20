50c93abcaeaef52cec9e7d184f594bc01dd0e192
Seven <freesunfury@gmail.com>
2016-07-20 15:40:45 -0500

    GameConstants cannot be instantiated

--- game/src/main/org/astraeus/game/GameConstants.java
+++ game/src/main/org/astraeus/game/GameConstants.java
@@ -11,57 +11,63 @@ import astraeus.game.model.location.impl.SquareArea;
  * 
  * @author Vult-R
  */
-public class GameConstants {
+public final class GameConstants {
 
-      /**
-       * The rate in milliseconds in which the game thread processes logic.
-       */
-      public static final int CYLCE_RATE = 600;
+	private GameConstants() {
 
-      /**
-       * The default spawn for entities.
-       */
-      public static final Position DEFAULT_LOCATION = new Position(3159, 3485);
+	}
 
-      /**
-       * The collection of areas that resemble the wilderness area.
-       */
-      public static final ImmutableList<Area> WILDERNESS =
-                  ImmutableList.of(new SquareArea("Wilderness", 2941, 3518, 3392, 3966));
+	/**
+	 * The rate in milliseconds in which the game thread processes logic.
+	 */
+	public static final int CYLCE_RATE = 600;
 
-      /**
-       * The collection of locations representing multi-combat areas.
-       */
-      public static final ImmutableList<Area> MULTIPLE_COMBAT = ImmutableList.of();
+	/**
+	 * The default spawn for entities.
+	 */
+	public static final Position DEFAULT_LOCATION = new Position(3159, 3485);
 
-      /**
-       * The maximum amount of players that can be logged in on a single game sequence.
-       */
-      public static final int LOGIN_LIMIT = 50;
+	/**
+	 * The collection of areas that resemble the wilderness area.
+	 */
+	public static final ImmutableList<Area> WILDERNESS = ImmutableList
+			.of(new SquareArea("Wilderness", 2941, 3518, 3392, 3966));
 
-      /**
-       * The maximum amount of players that can be logged in on a single game sequence.
-       */
-      public static final int LOGOUT_LIMIT = 50;
+	/**
+	 * The collection of locations representing multi-combat areas.
+	 */
+	public static final ImmutableList<Area> MULTIPLE_COMBAT = ImmutableList.of();
 
-      /**
-       * How far an npc will walk away from its origin.
-       */
-      public static final int NPC_RANDOM_WALK_DISTANCE = 5;
+	/**
+	 * The maximum amount of players that can be logged in on a single game
+	 * sequence.
+	 */
+	public static final int LOGIN_LIMIT = 50;
 
-      /**
-       * The distance in tiles that an npc will follow another entity.
-       */
-      public static final int NPC_FOLLOW_DISTANCE = 10;
+	/**
+	 * The maximum amount of players that can be logged in on a single game
+	 * sequence.
+	 */
+	public static final int LOGOUT_LIMIT = 50;
 
-      /**
-       * The maximum amount of players that can be held within the game world.
-       */
-      public static final int MAX_PLAYERS = 1024;
+	/**
+	 * How far an npc will walk away from its origin.
+	 */
+	public static final int NPC_RANDOM_WALK_DISTANCE = 5;
 
-      /**
-       * The maximum amount of mobs that can be held within the game world.
-       */
-      public static final int MAXIMUM_NPC_SPAWNS = 3000;
+	/**
+	 * The distance in tiles that an npc will follow another entity.
+	 */
+	public static final int NPC_FOLLOW_DISTANCE = 10;
+
+	/**
+	 * The maximum amount of players that can be held within the game world.
+	 */
+	public static final int MAX_PLAYERS = 1024;
+
+	/**
+	 * The maximum amount of mobs that can be held within the game world.
+	 */
+	public static final int MAXIMUM_NPC_SPAWNS = 3000;
 
 }
