8874e0d510a5110b6ce75d1a408034a51f2bdecb
Seven <freesunfury@gmail.com>
2016-07-21 18:50:11 -0500

    Documented GsonParser class.

--- util/src/main/org/astraeus/util/GsonParser.java
+++ util/src/main/org/astraeus/util/GsonParser.java
@@ -8,25 +8,75 @@ import com.google.gson.GsonBuilder;
 
 import java.util.logging.Logger;
 
+/**
+ * A specialized parser for parsing json files.
+ * 
+ * @author Vult-R
+ */
 public abstract class GsonParser<T> implements Runnable {
 
-	private final Logger logger = LoggerUtils.getLogger(GsonParser.class);
+	/**
+	 * The single logger for this class.
+	 */
+	private static final Logger LOGGER = LoggerUtils.getLogger(GsonParser.class);	
 
+	/**
+	 * The path to the file being parsed.
+	 */
 	protected String path;
 
+	/**
+	 * The flag that denotes to log this result to the output stream.
+	 */
 	private boolean log;
 
+	/**
+	 * Creates a new {@link GsonParser}.
+	 * 
+	 * @param path
+	 * 		The path of the file being parsed.
+	 */
 	public GsonParser(String path) {
 		this(path, true);
 	}
 
+	/**
+	 * Creates a new {@link GsonParser}.
+	 * 
+	 * @param path
+	 * 		The path of the file being parsed.
+	 * 
+	 * @param log
+	 * 		The flag to log this result.
+	 */
 	public GsonParser(String path, boolean log) {
 		this.path = path;
 		this.log = log;
 	}
 
+	/**
+	 * The method that will deserialize the json file into a java object.
+	 * 
+	 * @param gson
+	 * 		The gson object
+	 * 
+	 * @param reader
+	 * 		The reader that will parse the file.
+	 * 
+	 * @throws IOException
+	 * 
+	 * @return The type as an array.
+	 */
 	public abstract T[] deserialize(Gson gson, FileReader reader) throws IOException;
 
+	/**
+	 * The method called after the {@link #deserialize(Gson, FileReader)} has been called.
+	 * 
+	 * @param array
+	 * 		The array that was deserialized.
+	 * 
+	 * @throw IOException
+	 */
 	public abstract void onRead(T[] array) throws IOException;
 
 	@Override
@@ -41,7 +91,7 @@ public abstract class GsonParser<T> implements Runnable {
 			onRead(types);
 
 			if (log) {
-				logger.info("Loaded: " + types.length + " " + path.substring(path.lastIndexOf("/")  + 1).replaceAll("_", " "));
+				LOGGER.info("Loaded: " + types.length + " " + path.substring(path.lastIndexOf("/")  + 1).replaceAll("_", " "));
 			}
 
 		} catch (IOException e) {
